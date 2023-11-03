b8cad5d688c5841ec0050c4bc5059ac0c305cecb
Maysha-Khanom-Moon <117344645+Maysha-Khanom-Moon@users.noreply.github.com>
2023-11-03 09:40:25 +0000

    Data Type Summary

--- First.js
+++ First.js
@@ -1,45 +1,96 @@
-// console.log(2 > 1);
-// console.log(2 < 1);
-// console.log(2 >= 1);
-// console.log(2 <= 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+/*
+respect to store in memory and way to access --> there are 2 data types
+    1. primitive
+    2. non-primitive
 
 
-// comparison with different data types
+  # primitive:
+              --> they call by value
+    
+   => 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
 
-console.log("2" > 1);
-console.log("mn" > "mon")
 
+  # Reference (non primitve):
+                             --> they call by address
+    => Array, Objects, Functions
 
-// null
-console.log(null > 0); // flase
-console.log(null == 0); // false
-console.log(null >= 0); // true
+  
+** to make something unique -- we wrap that by Symbol
 
-// In javascript: comparison and equality check is different. So, they works differently
-// sometimes null => NaN or null => 0
-// so, null == 0 return false. But, null >= 0 return true
-
+*/
 
 /*
-for "use strict" : equality checker => '==='
+=> JavaScript dynamic language. 
+    -- hints: no need to declare the data type of variable
 
-actually its not only check the value, also check the datatype
-In this case, don't happen conversion automatically. 
+ -- variable declaration and data type declaration not same.
+    like: name = "moon" // variable not declared
+          let name = "moon" // data type not declared
 
-But, for '==': conversion happen automatically
-like: ("1" == 1) // true
-*/ 
+=> In "use strict" --> always we need to declare variable.
 
-
-// undefined
-console.log(undefined > 0); // false
-console.log(undefined == 0); // false
-console.log(undefined >= 0); // false
-// for undefined, every comparison return false. Because there is no value assigned
+*/  
 
 
+const score = 100 // number
+const scoreValue = 101.2 // Number
 
-// Remember: skip these types comparison. (null and undefined)
-// because, it creates confusion
+const isOkay = false // boolean
+const outsider = null // empty
+
+// *** for const --> always have to assigned value. otherwise it's a error ***
+
+
+let userEmail // undefined
+// or
+let user = undefined
+
+
+// ***** const do = undefined (error) *****
+
+
+
+// ********* Symbol *********
+// make something unique
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId); // false
+// because, they are unique
+
+
+
+// ********* BitInt **********
+const bigNumber = 2345678765432345678765234565432345n
+console.log(bigNumber); 
+
+// this last 'n' means BigInt.
+
+
+
+
+
+// ************* Array **************
+const heros = ['Captain America', 'Iron man', 'Hulk', 'Loki']
+
+
+// ************* Object *************
+let myObj = {
+    name: "Moon",
+    age: 21,
+    dept: 'CSE'
+}
+
+
+// ************ Function ************
+const myFunc = function(){
+    console.log("Hello world");
+}
+
+
+// ** data type detector: typeof
+console.log(typeof bigNumber); // bigint
+console.log(typeof null); // object
+console.log(typeof myFunc); // function (function object)
+console.log(typeof heros); // object
