e6f2a475c2c4e02d77873c3623dc6b059b32ccda
Maysha-Khanom-Moon <117344645+Maysha-Khanom-Moon@users.noreply.github.com>
2023-11-03 10:01:09 +0000

    Stack and Heap memory

--- First.js
+++ First.js
@@ -1,96 +1,32 @@
+// ************ memory *************
 /*
-respect to store in memory and way to access --> there are 2 data types
-    1. primitive
-    2. non-primitive
+ 2 types:
+        1. Stack(Primitive)
+                        ---> pass by value(copy)
 
-
-  # primitive:
-              --> they call by value
-    
-   => 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
-
-
-  # Reference (non primitve):
-                             --> they call by address
-    => Array, Objects, Functions
-
-  
-** to make something unique -- we wrap that by Symbol
-
+        2. Heap(Non-Primitve)
+                        ---> pass by referance(address)
 */
 
-/*
-=> JavaScript dynamic language. 
-    -- hints: no need to declare the data type of variable
+let first = 4
+let sec = first
+sec = 68;
+console.log(first); // 4
+console.log(sec); // 68
 
- -- variable declaration and data type declaration not same.
-    like: name = "moon" // variable not declared
-          let name = "moon" // data type not declared
-
-=> In "use strict" --> always we need to declare variable.
-
-*/  
+// they are primitive. so, pass the copy value. not address
 
 
-const score = 100 // number
-const scoreValue = 101.2 // Number
-
-const isOkay = false // boolean
-const outsider = null // empty
-
-// *** for const --> always have to assigned value. otherwise it's a error ***
-
-
-let userEmail // undefined
-// or
-let user = undefined
-
-
-// ***** const do = undefined (error) *****
-
-
-
-// ********* Symbol *********
-// make something unique
-
-const id = Symbol('123')
-const anotherId = Symbol('123')
-
-console.log(id == anotherId); // false
-// because, they are unique
-
-
-
-// ********* BitInt **********
-const bigNumber = 2345678765432345678765234565432345n
-console.log(bigNumber); 
-
-// this last 'n' means BigInt.
-
-
-
-
-
-// ************* Array **************
-const heros = ['Captain America', 'Iron man', 'Hulk', 'Loki']
-
-
-// ************* Object *************
-let myObj = {
-    name: "Moon",
-    age: 21,
-    dept: 'CSE'
+let funcOne = {
+    email: 'mkm123@gmail.com',
+    age: 19
 }
 
+let funcTwo = funcOne
+funcTwo.age = 21
 
-// ************ Function ************
-const myFunc = function(){
-    console.log("Hello world");
-}
+console.log(funcOne.age); // 21
+console.log(funcTwo.age); // 21
 
-
-// ** data type detector: typeof
-console.log(typeof bigNumber); // bigint
-console.log(typeof null); // object
-console.log(typeof myFunc); // function (function object)
-console.log(typeof heros); // object
+// they are non-primitve. so, pass the address of function. They have same address
+// so, funcTwo.value change means funcOne.value will change.
