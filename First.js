d245e6a636ddd4765c9b6e12683dd6fb6a3ab893
Maysha-Khanom-Moon <117344645+Maysha-Khanom-Moon@users.noreply.github.com>
2023-11-07 13:04:53 +0000

    All about String

--- First.js
+++ First.js
@@ -1,32 +1,100 @@
-// ************ memory *************
+const name = "moon"
+const repoCount = 15
+
+console.log(name + repoCount + " value");
+console.log('\n', "good ", 'girl', '\n')
+
+// concatenation by -->  '+' or ','
+
+
+
+// ************ Modern Way (string interpolation) ************
+
+// backtick => '``' 
+// `____ ${variable injection} ___`
+
+console.log(`hello! I am ${name}`);
+
+// -----------------------------------------------------------
+
+
+// string declaretion using object
+const gameName = new String("ninja Arashi")
+
+console.log(gameName[0]); // .charAt(0)
+console.log(gameName.charAt(0));
+
+console.log(gameName.__proto__); // all prototype included
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase()); 
+
+console.log(gameName.indexOf('a')); // if not present --> return: -1
+
+
+const subGame = gameName.substring(0, 4); // substring
+// in substring (-)index defined as 0
+
+console.log(subGame) // ninj
+console.log(gameName.substring(-2, 4)); // ninj
+
+
+// **************** slice *****************
+
+// it also can work by negative index
+// negative index works from reverse array
+
+console.log(gameName.slice(0, 4)); // as like substring
+console.log(gameName.slice(-10, 4)); // nj
+
 /*
- 2 types:
-        1. Stack(Primitive)
-                        ---> pass by value(copy)
+The array index at which the slice is to begin. If negative, 
+this argument specifies a position measured from the end of the array. 
 
-        2. Heap(Non-Primitve)
-                        ---> pass by referance(address)
-*/
-
-let first = 4
-let sec = first
-sec = 68;
-console.log(first); // 4
-console.log(sec); // 68
-
-// they are primitive. so, pass the copy value. not address
+That is, −1 indicates the last element, 
+         −2 indicates the next from the last element, and so on
+*/ 
 
 
-let funcOne = {
-    email: 'mkm123@gmail.com',
-    age: 19
-}
+// ******************* trim ********************
 
-let funcTwo = funcOne
-funcTwo.age = 21
+// trim(): stripped of whitespaces from both its beginning and end
 
-console.log(funcOne.age); // 21
-console.log(funcTwo.age); // 21
+const uni = "   RUET   "
+console.log(uni.trim());
 
-// they are non-primitve. so, pass the address of function. They have same address
-// so, funcTwo.value change means funcOne.value will change.
+// To return a new string with whitespace trimmed from just one end, 
+// use trimStart() or trimEnd().
+
+console.log(uni.trimEnd())
+console.log(uni.trimEnd) // [Function: trimEnd]
+
+
+
+
+// ******************* replace ********************
+
+const url = "https://moon.com/moon%20khan"
+console.log(url.replace('moon', 'mkm')); // first 'moon' will replaced by 'mkm'
+
+
+
+
+// ************* includes **************
+
+console.log(url.includes("mkm")); // false
+// check present or not
+
+
+
+// **************** split *****************
+
+const Name = "Maysha-Khanom-Moon"
+console.log(Name.split('-'));
+console.log(Name.split('-')[1]);
+
+
+// all works by copy
+
+// to know all prototype => goto inspect -> console 
