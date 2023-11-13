c6191bfe1d5d87324539df6929dcfff023e7c9b1
Maysha-Khanom-Moon <117344645+Maysha-Khanom-Moon@users.noreply.github.com>
2023-11-13 13:30:04 +0000

    Function, parameter with object and array

--- First.js
+++ First.js
@@ -1,41 +1,83 @@
-// de-structuring
+// ___________________ Functions ___________________
 
-const course = {
-    name: "Dev",
-    language: 'javaScript',
-    target: 'website',
-    teacher: 'hitesh'
+function sayName() {
+    console.log("moon");
 }
 
-// 1.
-console.log(course.teacher); // in general
-
-// 2.
-const {teacher} = course // de-structure of calls property
-console.log(teacher)
-
-// 3.
-const {teacher: sir} = course // de-structure with new name
-console.log(sir)
+// sayName: reference
+// (): execute
+sayName // just reference. no execution
+sayName() // reference and do execution
 
 
-// _______________ JSON ________________
-// just like object without its name
-// also, its keys and values both are will be string
-
-/*
-// 1. object formate
-{
-    "name": "moon",
-    "age": "21",
-    color: "yellow"
+function addTwo(num1, num2) {
+    return num1 + num2
+    let ans = num1 + num2 // not able to execute
 }
 
-// 2. array of objects format
-[
-    {...},
-    {...},
-    {...}
-]
+console.log(addTwo()) // NaN // no argument
+console.log(addTwo(2, 3));
 
-*/
+// danger
+console.log(addTwo(2, "a")); // 2a // for lake of argument specification
+
+
+// in conditional: undefined & null = false
+
+
+
+function temp(num) {
+    return num;
+}
+console.log(temp(44, 33, 49));
+
+// rest operator '...'
+function calculatePrice(...num) {
+    return num
+}
+
+console.log(calculatePrice(44, 33, 49)); // pass as an array
+
+
+//-------
+function temp1(val1, val2, ...num) {
+    return num;
+}
+
+console.log(temp1(11, 42, 13, 43));
+
+
+// function --> '...' (rest) 
+// object/array --> '...' (spread)
+
+
+
+// --------------- Object pass -----------------
+const user = {
+    email: "mkm@gmail.com",
+    price: 234
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.email} and price is ${anyobject.price}`);
+}
+
+// 1. by making object
+handleObject(user)
+
+// 2. without making object before
+handleObject({
+    email:"moon@google.com",
+    price: 234,
+    color: "blue"
+})
+
+
+// --------- array pass -------------
+const value = [34, 34, 3, 5, 4]
+
+function returnArray(getArray) {
+    return getArray[3]
+}
+
+console.log(returnArray(value));
